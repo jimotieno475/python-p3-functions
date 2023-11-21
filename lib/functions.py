c5f0fa4809a4becbe0adfba67cd0a6ed056983ee
jim <jim.otieno@student.moringaschool.com>
2023-11-21 08:55:41 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,54 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")  
 
+
+greet_programmer()    
+     
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+
+greet("jim")
+
 
 def greet_with_default(name="programmer"):
     pass
+    print (f"Hello, {name}!")
+
+
+greet_with_default()
+greet_with_default("jim")
 
 def add(num1, num2):
     pass
+    return num1+num2
 
 def halve(number):
     pass
+    return number/2
+
+def my_function(param):
+    print("Running my_function")
+    return param + 1   
+
+
+my_function(6)
+
+
+def say_hi(name="Engineer"):
+    print(f"Hi there, {name}!")
+
+
+say_hi()
+
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle
+    print(best_hairstyle)
+
+
+
+stylish_painter()
